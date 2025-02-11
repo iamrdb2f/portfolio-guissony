e652c2beb470e8d7515be529c694cf5d2f7139ad
iamrdb2f <contact@desbauford.com>
2025-02-11 06:56:55 +0100

    fixing project configuraiton

--- src/app/components/card-project/card-project.component.ts
+++ src/app/components/card-project/card-project.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-card-project',
-  templateUrl: './card-project.component.html',
-  styleUrls: ['./card-project.component.scss']
+    selector: 'app-card-project',
+    templateUrl: './card-project.component.html',
+    styleUrls: ['./card-project.component.scss'],
+    standalone: false
 })
 export class CardProjectComponent {
 
--- src/app/components/modal-project/modal-project.component.ts
+++ src/app/components/modal-project/modal-project.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-modal-project',
-  templateUrl: './modal-project.component.html',
-  styleUrls: ['./modal-project.component.scss']
+    selector: 'app-modal-project',
+    templateUrl: './modal-project.component.html',
+    styleUrls: ['./modal-project.component.scss'],
+    standalone: false
 })
 export class ModalProjectComponent {
 
