7dd9cb1b3d8807e742e7b6609b32b3c19138baae
iamrdb2f <contact@desbauford.com>
2025-02-11 13:46:53 +0100

    Ajout du lazy loading pour les nouvelles pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component'; // Ajout
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }, // Page d'accueil
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'competences', loadChildren: () => import('./pages/competences/competences.module').then(m => m.CompetencesModule) },
+  { path: 'services', loadChildren: () => import('./pages/services/services.module').then(m => m.ServicesModule) },
   { path: 'portfolio', loadChildren: () => import('./pages/portfolio/portfolio.module').then(m => m.PortfolioModule) },
   { path: 'blog', loadChildren: () => import('./pages/blog/blog.module').then(m => m.BlogModule) },
   { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
-  { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule) }
+  { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule) },
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Redirection vers l'accueil en cas de mauvaise URL
 ];
 
 @NgModule({
