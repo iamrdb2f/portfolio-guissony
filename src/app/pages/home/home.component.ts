7be478c7369c9515ca2d0a0c55206dbf868e101d
iamrdb2f <contact@desbauford.com>
2025-02-12 13:53:00 +0100

    Update on the new version of the home page, with animation and better display.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
-    standalone: false
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  texts: string[] = ['JAVA & SPRING EXPERT', 'SELENIUM AUTOMATION SPECIALIST', 'ENKINS & DEVOPS ENTHUSIASTIC'];
+  currentText = '';
+  currentIndex = 0;
+  typingSpeed = 100;
+  displayTime = 3000;
 
+  ngOnInit() {
+    this.startTextAnimation();
+  }
+
+  startTextAnimation() {
+    let i = 0;
+
+    const typeText = () => {
+      if (i < this.texts[this.currentIndex].length) {
+        this.currentText += this.texts[this.currentIndex].charAt(i);
+        i++;
+        setTimeout(typeText, this.typingSpeed);
+      } else {
+        setTimeout(() => this.deleteText(), this.displayTime);
+      }
+    };
+
+    typeText();
+  }
+
+  deleteText() {
+    if (this.currentText.length > 0) {
+      this.currentText = this.currentText.slice(0, -1);
+      setTimeout(() => this.deleteText(), this.typingSpeed);
+    } else {
+      this.currentIndex = (this.currentIndex + 1) % this.texts.length;
+      this.startTextAnimation();
+    }
+  }
 }
---
+++ src/app/pages/home/typing-animation.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  textArray: string[] = [
+    "JAVA & SPRING EXPERT",
+    "SELENIUM AUTOMATION SPECIALIST",
+    "JENKINS & DEVOPS ENTHUSIASTIC"
+  ];
+  typingSpeed: number = 100;
+  erasingSpeed: number = 50;
+  delayBetweenTexts: number = 2000;
+  currentTextIndex: number = 0;
+  currentText: string = "";
+  isDeleting: boolean = false;
+
+  ngOnInit() {
+    this.typingEffect();
+  }
+
+  typingEffect() {
+    const fullText = this.textArray[this.currentTextIndex];
+    if (this.isDeleting) {
+      this.currentText = fullText.substring(0, this.currentText.length - 1);
+    } else {
+      this.currentText = fullText.substring(0, this.currentText.length + 1);
+    }
+
+    setTimeout(() => {
+      if (!this.isDeleting && this.currentText === fullText) {
+        setTimeout(() => {
+          this.isDeleting = true;
+          this.typingEffect();
+        }, this.delayBetweenTexts);
+      } else if (this.isDeleting && this.currentText === "") {
+        this.isDeleting = false;
+        this.currentTextIndex = (this.currentTextIndex + 1) % this.textArray.length;
+        this.typingEffect();
+      } else {
+        this.typingEffect();
+      }
+    }, this.isDeleting ? this.erasingSpeed : this.typingSpeed);
+  }
+}
